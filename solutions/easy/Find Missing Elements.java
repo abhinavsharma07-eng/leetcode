e247a48746304431420d908d84fb1bdfc188986d
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-05 13:21:13 +0530

    Add solution for Find Missing Elements

---
+++ solutions/easy/Find Missing Elements.java
@@ -0,0 +1,24 @@
+// Title: Find Missing Elements
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-missing-elements/
+
+            if(!set.contains(i))  list.add(i);
+        for(int j=0,i =lowest ; i<= largest ;i++ ){
+        }
+            if(n < lowest ) lowest = n;
+             
+        }
+        return list;
+    }
+            if(n > largest ) largest = n;
+            set.add(n);
+        for(int n : nums){
+
+        int largest = Integer.MIN_VALUE;
+}
+        int lowest = Integer.MAX_VALUE;
+        Set<Integer> set = new HashSet<>();
+        List<Integer> list = new ArrayList<>();
+    public List<Integer> findMissingElements(int[] nums) {
+class Solution {
