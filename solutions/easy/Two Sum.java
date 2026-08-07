67edd752ec84d609093b72ff0f5ab9bf40d75fef
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-07 11:31:47 +0530

    Add solution for Two Sum

--- solutions/easy/Two Sum.java
+++ solutions/easy/Two Sum.java
@@ -3,15 +3,4 @@
             // Language: Java
             // Link: https://leetcode.com/problems/two-sum/
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        
-        int [] arr = new int[2];
-         for(int j = 0; j <nums.length ; j++){
-            for(int i = j +1 ; i < nums.length;i++){
-                if (nums[j] + nums[i]== target){
-                    return new  int[]{j ,i};
-                }
-            }
-  
-        } 
+c
