ab1fdf21ea0e5ea32505acc6bab85ba7a7489735
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-09 10:39:13 +0530

    Add solution for Categorize Box According to Criteria

---
+++ solutions/easy/Categorize Box According to Criteria.java
@@ -0,0 +1,6 @@
+// Title: Categorize Box According to Criteria
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/categorize-box-according-to-criteria/
+
+c
