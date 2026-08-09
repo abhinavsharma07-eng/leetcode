8bbe8cb07236138ceb9e7e789b6ef8cf85e43576
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-09 10:44:17 +0530

    Implement categorizeBox method for box classification

--- solutions/easy/Categorize Box According to Criteria.java
+++ solutions/easy/Categorize Box According to Criteria.java
@@ -3,4 +3,13 @@
             // Language: Java
             // Link: https://leetcode.com/problems/categorize-box-according-to-criteria/
 
-c
+    public String categorizeBox(int length, int width, int height, int mass) {
+                
+        long volume = (long)length*width*height;
+        int dim = (length>=width)?((length>=height)?length:height):((width>=height)?width:height);
+        if((volume >= 1000_000_000L||dim>=100_00)&& mass>=100) return "Both";
+        else if (volume >= 1000_000_000L ||dim>=100_00) return "Bulky";
+        else if (mass >=100) return "Heavy";
+        else return "Neither";  
+    }
+
