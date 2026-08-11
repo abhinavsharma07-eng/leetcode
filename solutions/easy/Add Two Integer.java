3f0cbeb3294884b88ed85616717befb8b1012165
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-11 23:52:27 +0530

    Add "Add Two Integer
    Updated title and link for the Add Two Integer problem. Added sum method to return the sum of two integers.

---
+++ solutions/easy/Add Two Integer.java
@@ -0,0 +1,9 @@
+// Title: Add Two Integer
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/add-two-integer/
+
+    public int sum(int num1, int num2) {
+        return num1+num2;
+        
+    }
