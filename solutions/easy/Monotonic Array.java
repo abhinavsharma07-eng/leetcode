ea8d3467c91a54b253cf657042bba192786b4496
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-12 23:39:10 +0530

    Correct isMonotonic method implementation
    Fix logic to return true for monotonic arrays.

--- solutions/easy/Monotonic Array.java
+++ solutions/easy/Monotonic Array.java
@@ -3,13 +3,16 @@
             // Language: Java
             // Link: https://leetcode.com/problems/monotonic-array/
 
-class Solution {
-    public boolean isMonotonic(int[] nums) {
-        for(int i =0 ;i< nums.length-1;i++){
-            if( nums[0] <= nums[nums.length-1]){
-            if(nums[i]> nums[i+1]) return false;
-            }
-        }
-            else {
-                if(nums[i]< nums[i+1]) return false;
-            }
+class Solution {
+    public boolean isMonotonic(int[] nums) {
+        for(int i =0 ;i< nums.length-1;i++){
+            if( nums[0] <= nums[nums.length-1]){
+            if(nums[i]> nums[i+1]) return false;
+            }
+            else {
+                if(nums[i]< nums[i+1]) return false;
+            }
+        }
+        return true;
+    }
+}
