cd416fbebb9bc6a78a976d6b72fc947531990abe
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-13 23:39:06 +0530

    Add solution for Sign of the Product of an Array

---
+++ solutions/easy/Sign of the Product of an Array.java
@@ -0,0 +1,17 @@
+// Title: Sign of the Product of an Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/sign-of-the-product-of-an-array/
+
+class Solution {
+    public int arraySign(int[] nums) {
+        int count=0;
+        for(int i =0; i< nums.length; i++){
+            if(nums[i]==0) return 0;
+            if(nums[i]<0) count++;
+        }
+        if(count%2==0)return 1;
+        return -1;
+        
+    }
+}
