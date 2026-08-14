99b6ac56874064f818c1e1f70d3a8716f24c520d
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-14 23:13:33 +0530

    Refactor Climbing Stairs solution with memoization

--- solutions/easy/Climbing Stairs.java
+++ solutions/easy/Climbing Stairs.java
@@ -3,16 +3,18 @@
             // Language: Java
             // Link: https://leetcode.com/problems/climbing-stairs/
 
-        if (n == 0 || n == 1) {
-            return 1;
-        }
-        if (!memo.containsKey(n)) {
-            memo.put(n, climbStairs(n-1, memo) + climbStairs(n-2, memo));
-        }
-        return memo.get(n);
-    }
+     class Solution {
+    public int climbStairs(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return climbStairs(n, memo);
+    }
+    private int climbStairs(int n, Map<Integer, Integer> memo) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        if (!memo.containsKey(n)) {
+            memo.put(n, climbStairs(n-1, memo) + climbStairs(n-2, memo));
+        }
+        return memo.get(n);
+    }
 }
-    private int climbStairs(int n, Map<Integer, Integer> memo) {
-    }
-        return climbStairs(n, memo);
-        Map<Integer, Integer> memo = new HashMap<>();
