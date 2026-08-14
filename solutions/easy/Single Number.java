02ebbcb73d5e5d1d1119222c3edacf5dcf169d4b
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-14 23:56:11 +0530

    Add solution for Single Number

---
+++ solutions/easy/Single Number.java
@@ -0,0 +1,14 @@
+// Title: Single Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/single-number/
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        for(int n: nums){
+        }
+    }
+        int result =0;
+            result = result^n;
+        return result;
+}
