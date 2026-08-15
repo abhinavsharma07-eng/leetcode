7fa660300a484a4f6775667c6e1edf7d7e79f864
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-15 22:34:34 +0530

    Refactor moveZeroes method to handle zeroes correctly

--- solutions/easy/Move Zeroes.java
+++ solutions/easy/Move Zeroes.java
@@ -3,21 +3,21 @@
             // Language: Java
             // Link: https://leetcode.com/problems/move-zeroes/
 
-                    int temp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = temp;
-                else{
-                if(nums[j]==0) i--; 
-            if(nums[i]==0){
-       while(j<nums.length){
-       int j =1;
-       int i =0;
-                }
-                }
-                i++;
-class Solution {
-    public void moveZeroes(int[] nums) {
-                j++;
-        }
-    }
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int i =0;
+       int j =1;
+       while(j<nums.length){
+            if(nums[i]==0){
+                if(nums[j]==0) i--; 
+                else{
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                }
+                }
+                i++;
+                j++;
+        }
+    }
 }
