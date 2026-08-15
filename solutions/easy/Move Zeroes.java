7c85ca9358859ac2eb213bbcfce9822afe7e1909
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-15 22:32:44 +0530

    Add solution for Move Zeroes

---
+++ solutions/easy/Move Zeroes.java
@@ -0,0 +1,23 @@
+// Title: Move Zeroes
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/move-zeroes/
+
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                else{
+                if(nums[j]==0) i--; 
+            if(nums[i]==0){
+       while(j<nums.length){
+       int j =1;
+       int i =0;
+                }
+                }
+                i++;
+class Solution {
+    public void moveZeroes(int[] nums) {
+                j++;
+        }
+    }
+}
