ad9dfbab9290ecbfdca293f7a565eba8fa975912
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-16 23:04:18 +0530

    Add solution for Merge Sorted Array

---
+++ solutions/easy/Merge Sorted Array.java
@@ -0,0 +1,14 @@
+// Title: Merge Sorted Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/merge-sorted-array/
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+        for(int i = m ; i<(m+n);i++){
+        }
+    }
+            nums1[i] = nums2[i-m];
+        Arrays.sort(nums1);
+}
