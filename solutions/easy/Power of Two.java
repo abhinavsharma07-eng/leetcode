953fed8cd1b8014f1eed77c5a54d7f3e2eda7b5c
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-19 23:14:13 +0530

    Add solution for Power of Two

---
+++ solutions/easy/Power of Two.java
@@ -0,0 +1,12 @@
+// Title: Power of Two
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/power-of-two/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        
+    }
+        if(((n & (n-1)) == 0)&& n>0) return true;
+        return false;
+}
