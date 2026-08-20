5c5113b04964dea98c78248da37a31dc422832b5
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-20 21:23:24 +0530

    Refactor majorityElement logic in Solution class
    Refactor majorityElement method to correctly handle count reset and element assignment.

--- solutions/easy/Majority Element.java
+++ solutions/easy/Majority Element.java
@@ -3,18 +3,18 @@
             // Language: Java
             // Link: https://leetcode.com/problems/majority-element/
 
-         int x = nums[0];
-        int count =0;
-        for(int n : nums ){
-            if(x == n) count++;
-            else count--;
-            
-        }
-        return x;
+class Solution {
+    public int majorityElement(int[] nums) {
+        
+         int x = nums[0];
+        int count =0;
+        for(int n : nums ){
+            if(count ==0 ) x = n;
+            if(x == n) count++;
+            else count--;
+            
+        }
+        return x;
 
-    }
-        
-class Solution {
-    public int majorityElement(int[] nums) {
-            if(count ==0 ) x = n;
+    }
 }
