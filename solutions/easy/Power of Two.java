e7b57cf888656d059d0919d4414d49c91e124eb2
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-20 21:24:13 +0530

    Simplify isPowerOfTwo method implementation

--- solutions/easy/Power of Two.java
+++ solutions/easy/Power of Two.java
@@ -3,10 +3,8 @@
             // Language: Java
             // Link: https://leetcode.com/problems/power-of-two/
 
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        
-    }
-        if(((n & (n-1)) == 0)&& n>0) return true;
-        return false;
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return (n&(n-1)) == 0&& n>0;
+    }
 }
