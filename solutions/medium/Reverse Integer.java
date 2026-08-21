7e099b0412cb12d97be9080a297fcd4d74129a71
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-21 22:56:00 +0530

    Implement reverse integer function in Java

--- solutions/medium/Reverse Integer.java
+++ solutions/medium/Reverse Integer.java
@@ -3,4 +3,16 @@
             // Language: Java
             // Link: https://leetcode.com/problems/reverse-integer/
 
-c
+class Solution {
+    public int reverse(int x) {
+        long sum = 0; 
+        while (x != 0) {
+            sum = sum * 10 + x % 10;
+            x /= 10;
+        }
+        if (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
+            return 0;
+        }
+        return (int) sum;
+    }
+}
