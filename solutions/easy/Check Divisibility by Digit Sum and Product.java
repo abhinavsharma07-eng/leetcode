281d07a8370c350075c9057b362073b4906e7ece
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-22 22:34:09 +0530

    Add solution for Check Divisibility by Digit Sum and Product

---
+++ solutions/easy/Check Divisibility by Digit Sum and Product.java
@@ -0,0 +1,20 @@
+// Title: Check Divisibility by Digit Sum and Product
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/
+
+class Solution {
+    public boolean checkDivisibility(int n) {
+        int sum =0;
+        while(n!=0){
+            sum+=d;
+            n/=10;
+        }
+    }
+        if(num%(mul+sum)==0) return true;
+            int d = n%10;
+            mul *=d;
+        int mul =1;
+        return false;
+        int num =n;
+}
