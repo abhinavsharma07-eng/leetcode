876bddeca7594a7dda2bd4d3e91bdeff82bbd13a
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-26 12:21:03 +0530

    Add solution for Check Divisibility by Digit Sum and Product

---
+++ solutions/easy/Check Divisibility by Digit Sum and Product.c
@@ -0,0 +1,6 @@
+// Title: Check Divisibility by Digit Sum and Product
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/check-divisibility-by-digit-sum-and-product/
+
+b
