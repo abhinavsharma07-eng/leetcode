a19973dced24c6d90961c0c75612b747ab1195ae
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-26 23:00:40 +0530

    Add solution for Count the Digits That Divide a Number

---
+++ solutions/easy/Count the Digits That Divide a Number.c
@@ -0,0 +1,15 @@
+// Title: Count the Digits That Divide a Number
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+
+    while(num!=0){
+        num/=10;
+    }
+    int count=0;
+    return count;
+        int d = num%10;
+        if(n%d==0) count++;
+}
+    int n =num;
+int countDigits(int num) {
