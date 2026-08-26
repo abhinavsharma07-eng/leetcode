026acf697c7aca2f6d8eac92c633a8c517b8153f
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-26 12:07:36 +0530

    Add solution for Move Zeroes

--- solutions/easy/Move Zeroes.c
+++ solutions/easy/Move Zeroes.c
@@ -3,4 +3,18 @@
             // Language: C
             // Link: https://leetcode.com/problems/move-zeroes/
 
-v
+     int nonZero=0;
+void moveZeroes(int* nums, int numsSize) {
+     int i =0;
+     for(i =0 ;i< numsSize;i++){
+        if(nums[i]!=0){
+            nums[nonZero]= nums[i];
+            nonZero++;
+        }
+     }
+        for(i = nonZero;i<numsSize;i++)
+            nums[i]=0;
+     }
+
+    
+
