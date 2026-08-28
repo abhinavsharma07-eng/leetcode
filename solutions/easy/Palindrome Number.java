4d94fe7f5cdd6ce572fc1d2dc04bb0ed0f14cc0e
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-28 22:44:48 +0530

    Add solution for Palindrome Number

--- solutions/easy/Palindrome Number.java
+++ solutions/easy/Palindrome Number.java
@@ -3,15 +3,4 @@
             // Language: Java
             // Link: https://leetcode.com/problems/palindrome-number/
 
-class Solution {
-    public boolean isPalindrome(int x) {
-        int m=x;
-        int rev=0,r;  
-        if (x < 0) return  false;
-        else {
-            while (m!=0){
-                r= m%10;
-                rev = rev*10 + r;
-                m = m/10;
-                
-            }
+c
