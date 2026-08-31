919242abd2c4c54e21a3eee86859dacb914f5d14
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-31 23:29:12 +0530

    Add solution for Harshad Number

---
+++ solutions/easy/Harshad Number.java
@@ -0,0 +1,17 @@
+// Title: Harshad Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/harshad-number/
+
+class Solution {
+    public int sumOfTheDigitsOfHarshadNumber(int x) {
+        int sum =0;
+        int n =x;
+        while(x!=0){
+            sum +=x%10;
+            x/=10;
+        }
+        if(n%sum ==0) return sum;
+        return -1;   
+    }
+}
