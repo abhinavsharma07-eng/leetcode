6c0b0ca92acb886f97a20720f95036a2d484422f
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-31 23:32:20 +0530

    Add solution for Count the Digits That Divide a Number

---
+++ solutions/easy/Count the Digits That Divide a Number.java
@@ -0,0 +1,6 @@
+// Title: Count the Digits That Divide a Number
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/count-the-digits-that-divide-a-number/
+
+ 
