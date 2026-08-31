51c6506c9f3721bd0a16425adca8119c4b036f7b
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-31 22:46:56 +0530

    Add solution for Subtract the Product and Sum of Digits of an Integer

---
+++ solutions/easy/Subtract the Product and Sum of Digits of an Integer.c
@@ -0,0 +1,14 @@
+// Title: Subtract the Product and Sum of Digits of an Integer
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/subtract-the-product-and-sum-of-digits-of-an-integer/
+
+        mul*=d;
+    }
+        n /=10;
+    int mul = 1;
+    while(n!=0){
+        int d = n%10;
+        sum +=d;
+    int sum =0;
+int subtractProductAndSum(int n) {
