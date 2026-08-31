36d9e209020fbead06772a4e6f92f8a8219f8d75
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-31 11:00:41 +0530

    Add solution for Reverse String

---
+++ solutions/easy/Reverse String.c
@@ -0,0 +1,12 @@
+// Title: Reverse String
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/reverse-string/
+
+        s[i]=s[sSize -i-1];
+        s[sSize -i-1] = temp;
+    }
+}
+        char temp= s[i];
+    for(int i =0 ; i <sSize/2 ;i++){
+void reverseString(char* s, int sSize) {
