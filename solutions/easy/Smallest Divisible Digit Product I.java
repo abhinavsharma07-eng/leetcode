1b9858c74f6281a8a290446f619f86418ac08964
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-31 23:31:52 +0530

    Add solution for Smallest Divisible Digit Product I

---
+++ solutions/easy/Smallest Divisible Digit Product I.java
@@ -0,0 +1,17 @@
+// Title: Smallest Divisible Digit Product I
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/smallest-divisible-digit-product-i/
+
+    int num =n;
+    int mul = 1;
+    while(num !=0){
+        mul *=num%10;
+        num /=10;
+    }
+    if(mul%t==0) return n;
+    else return smallestNumber(n+1,t);
+    }
+}
+    public int smallestNumber(int n, int t) {
+class Solution {
