62813650d1d0c0f9db8a70478bf48d57e115974a
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-08-31 10:19:33 +0530

    Add solution for Maximum Product of Two Digits

---
+++ solutions/easy/Maximum Product of Two Digits.java
@@ -0,0 +1,14 @@
+// Title: Maximum Product of Two Digits
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-product-of-two-digits/
+
+            }
+                num /=10;
+                if(((num%10)*(n%10))>max) max = (num%10)*(n%10);
+        while(n!=0){
+            int num = n/10;
+            while(num!=0){
+    public int maxProduct(int n) {
+        int max= Integer.MIN_VALUE ;
+class Solution {
