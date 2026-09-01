0e07f9b23264270c17ee1252da545681dfe5bbe3
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-01 22:57:14 +0530

    Add solution for Sort Array By Parity

---
+++ solutions/easy/Sort Array By Parity.c
@@ -0,0 +1,14 @@
+// Title: Sort Array By Parity
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/sort-array-by-parity/
+
+    for(int i = 0; i < numsSize;i++){
+    int pointer=0;
+    *returnSize = numsSize;
+{
+int* sortArrayByParity(int* nums, int numsSize, int* returnSize) 
+ */
+ calls free().
+ * Note: The returned array must be malloced, assume caller 
+/**
