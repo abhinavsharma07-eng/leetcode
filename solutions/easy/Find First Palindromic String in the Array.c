66c43dc2e8c4c346d7a61d2d07610dfa723494b4
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-01 23:35:03 +0530

    Add solution for Find First Palindromic String in the Array

---
+++ solutions/easy/Find First Palindromic String in the Array.c
@@ -0,0 +1,6 @@
+// Title: Find First Palindromic String in the Array
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+
+b
