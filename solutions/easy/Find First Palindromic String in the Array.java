d0b631d4b82fb924bc62a1c13f350d22fdc49760
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-01 23:20:26 +0530

    Add solution for Find First Palindromic String in the Array

---
+++ solutions/easy/Find First Palindromic String in the Array.java
@@ -0,0 +1,15 @@
+// Title: Find First Palindromic String in the Array
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-first-palindromic-string-in-the-array/
+
+    public boolean isPalrindromic(String word){
+        String rev = new StringBuilder(word).reverse().toString
+        ();
+        return rev.equals(word);
+    }
+    }
+        return ""; 
+        }
+            if(isPalrindromic(words[i])) return words[i];
+        for(int i=0 ; i < words.length ; i++){
