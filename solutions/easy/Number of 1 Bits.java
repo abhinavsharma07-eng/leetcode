a1035141abe4001ace3fae05c6731c2a9b915a18
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-02 22:57:51 +0530

    Add solution for Number of 1 Bits

---
+++ solutions/easy/Number of 1 Bits.java
@@ -0,0 +1,15 @@
+// Title: Number of 1 Bits
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/number-of-1-bits/
+
+class Solution {
+    public int hammingWeight(int n) {
+        int count=0;
+        while(n!=0){
+            if(n%2==1) count++;
+            n/=2;
+        }
+        return count;
+    }
+}
