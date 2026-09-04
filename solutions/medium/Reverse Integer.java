c811140e3f067a6882485fc86192985c46288d67
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-05 00:11:33 +0530

    Add solution for Reverse Integer

--- solutions/medium/Reverse Integer.java
+++ solutions/medium/Reverse Integer.java
@@ -3,16 +3,4 @@
             // Language: Java
             // Link: https://leetcode.com/problems/reverse-integer/
 
-class Solution {
-    public int reverse(int x) {
-        long sum = 0; 
-        while (x != 0) {
-            sum = sum * 10 + x % 10;
-            x /= 10;
-        }
-        if (sum > Integer.MAX_VALUE || sum < Integer.MIN_VALUE) {
-            return 0;
-        }
-        return (int) sum;
-    }
-}
+c
