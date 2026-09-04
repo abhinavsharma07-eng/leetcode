70ede7243e168809685284c96b4e5ebb084f442f
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-05 00:00:36 +0530

    Add solution for Smallest Stable Index I

--- solutions/easy/Smallest Stable Index I.c
+++ solutions/easy/Smallest Stable Index I.c
@@ -3,14 +3,4 @@
             // Language: C
             // Link: https://leetcode.com/problems/smallest-stable-index-i/
 
-    int max =nums[0];
-    for(int i = 0 ; i < numsSize ; i++){
-        if(max<nums[i]) max =nums[i];
-        int min = INT_MAX;
-        for(int j =i ;j < numsSize;j++){
-            if(nums[j]<min) min = nums[j];
-        }
-        if((max-min) <= k) return i;
-    }   
-    return -1;
-}
+i
