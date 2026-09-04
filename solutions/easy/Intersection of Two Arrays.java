0fd023bfa69a2a77fcb7e5a3c7bc40afd69944ec
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-05 00:37:49 +0530

    Add solution for Intersection of Two Arrays

---
+++ solutions/easy/Intersection of Two Arrays.java
@@ -0,0 +1,12 @@
+// Title: Intersection of Two Arrays
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/intersection-of-two-arrays/
+
+                 set.add(n);
+                }
+            }
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
