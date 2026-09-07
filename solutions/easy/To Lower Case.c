afcf6ee189d36aedb746c6bd08ba0cba74eacfda
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-07 22:40:01 +0530

    Add solution for To Lower Case

---
+++ solutions/easy/To Lower Case.c
@@ -0,0 +1,14 @@
+// Title: To Lower Case
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/to-lower-case/
+
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+    }
+            s[i] = s[i] + 32;
+        }
+    return s;
+    int len = strlen(s);
+    for (int i = 0; i < len; i++) {
+}
+char* toLowerCase(char* s) {
