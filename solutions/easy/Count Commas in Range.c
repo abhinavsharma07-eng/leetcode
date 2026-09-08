7316105869810cf8b6424bb18fe9447ce539148e
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-08 23:02:44 +0530

    Add solution for Count Commas in Range

---
+++ solutions/easy/Count Commas in Range.c
@@ -0,0 +1,9 @@
+// Title: Count Commas in Range
+            // Difficulty: Easy
+            // Language: C
+            // Link: https://leetcode.com/problems/count-commas-in-range/
+
+int countCommas(int n) {
+    if(n<1000) return 0;
+    else return n -999;
+}
