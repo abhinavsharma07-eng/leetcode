fe6ca9a06472ead8ca345789b2907115a51b67b8
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-09 12:24:51 +0530

    Add solution for Delete Node in a Linked List

---
+++ solutions/medium/Delete Node in a Linked List.c
@@ -0,0 +1,16 @@
+// Title: Delete Node in a Linked List
+            // Difficulty: Medium
+            // Language: C
+            // Link: https://leetcode.com/problems/delete-node-in-a-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+    node->val = node->next->val;
+    node->next = node->next->next;
+}
