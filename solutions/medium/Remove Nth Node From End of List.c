9538d811664a8f48386786ed35d82d8e1a909b14
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-21 20:23:22 +0530

    Add solution for Remove Nth Node From End of List

---
+++ solutions/medium/Remove Nth Node From End of List.c
@@ -0,0 +1,6 @@
+// Title: Remove Nth Node From End of List
+            // Difficulty: Medium
+            // Language: C
+            // Link: https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+ 
