02e8accf4f4ea3d1ebb55590646844327d07dc91
Abhinav Sharma <234881677+abhinavsharma07-eng@users.noreply.github.com>
2026-09-24 23:21:53 +0530

    Add solution for Fibonacci Number

--- solutions/easy/Fibonacci Number.java
+++ solutions/easy/Fibonacci Number.java
@@ -3,4 +3,12 @@
             // Language: Java
             // Link: https://leetcode.com/problems/fibonacci-number/
 
-i
+class Solution {
+    public int fib(int n) {
+        if(n ==0 ) return 0;
+        if(n == 1) return 1;
+        Map<Integer,Integer> set= new HashMap<>(); 
+            if(set.get(n)!= null) return set.get(n);
+        return fib(n-1)+fib(n-2);
+    }
+}
